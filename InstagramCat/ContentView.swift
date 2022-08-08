93f08f6069a4e7d26878d0d449a92a441e1bac24
hsiogi <hsiogi@168@gmail.com>
2022-08-08 16:13:04 +0800

    init

---
+++ InstagramCat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramCat
+//
+//  Created by 林品均 on 2022/8/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstagramCat/InstagramCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCatApp.swift
+//  InstagramCat
+//
+//  Created by 林品均 on 2022/8/8.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
