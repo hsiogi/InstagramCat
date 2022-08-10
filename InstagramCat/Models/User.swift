962958b656c39405d5a02f2ded295ba0c9601120
hsiogi <hsiogi@168@gmail.com>
2022-08-10 11:29:49 +0800

    新增 使用者資料

---
+++ InstagramCat/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  InstagramCat
+//
+//  Created by 林品均 on 2022/8/8.
+//
+
+import Foundation
+
+struct User : Codable{
+    var thumbNail : String
+    var account : String
+   
+}
+
