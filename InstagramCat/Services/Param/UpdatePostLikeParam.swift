cc971c815ddb760b5981f41f2b748c6e68a1e3f7
hsiogi <hsiogi@168@gmail.com>
2022-08-16 17:55:29 +0800

    新增按讚參數

---
+++ InstagramCat/Services/Param/UpdatePostLikeParam.swift
@@ -0,0 +1,15 @@
+//
+//  UpdatePostLikeParam.swift
+//  InstagramCat
+//
+//  Created by 林品均 on 2022/8/16.
+//
+
+import Foundation
+
+struct UpdatePostLikeParam : Codable{
+    
+    var userId: String
+    var value: Bool
+    
+}
