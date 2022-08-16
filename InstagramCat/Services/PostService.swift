264fba857e2e36328d32e80ede7da479171a1c6e
hsiogi <hsiogi@168@gmail.com>
2022-08-16 17:55:55 +0800

    新增按讚功能在postservice＆調整對應測試

--- InstagramCat/Services/PostService.swift
+++ InstagramCat/Services/PostService.swift
@@ -10,11 +10,33 @@ import Foundation
 
 protocol PostServiceProtocol{
     func GetPosts(userId: String, pageIndex: Int, pageCount: Int) async throws -> [Post]
+    
+    func UpdatePostLike(userId: String, postId: String, liked: Bool) async throws
 }
 
 
 
 class PostService : PostServiceProtocol{
+    
+    
+    func UpdatePostLike(userId: String, postId: String, liked: Bool) async throws {
+        if let url = URL(string: "http://localhost:5079/Post/\(postId)/interaction"){
+            var request = URLRequest(url: url)
+            request.httpMethod = "PUT"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            let param = UpdatePostLikeParam(userId: userId, value: liked)
+            
+            let encoder = JSONEncoder()
+            let jsonData = try encoder.encode(param)
+            
+            request.httpBody = jsonData
+            
+            let (_, _) = try await URLSession.shared.data(for: request)
+            
+        }
+    }
+    
     
     
     
@@ -31,7 +53,7 @@ class PostService : PostServiceProtocol{
                 
                 return try decoder.decode([Post].self, from: data)
             }
-        
+            
         }
         else{
             throw URLError(URLError.badURL)
--- InstagramCatTests/PostServiceTests.swift
+++ InstagramCatTests/PostServiceTests.swift
@@ -1,45 +1,51 @@
 //
-//  InstagramCatTests.swift
+//  PostServiceTests.swift
 //  InstagramCatTests
 //
-//  Created by 林品均 on 2022/8/12.
+//  Created by 林品均 on 2022/8/16.
 //
 
 import XCTest
 
+@testable import InstagramCat
 
 class PostServiceTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func getPosts() async throws {
+    
+   
+    func test_getposts() async throws {
         
-        var service = PostService()
+        let service = PostService()
         
         
         
         do{
+            print("run")
             var posts = try await service.GetPosts(userId: "62f503aeaa188d05520baa31", pageIndex: 1, pageCount: 5)
             
-           
             
-        }catch{ 
+            
+        }catch{
             XCTFail("error occurs. \(error)")
         }
-        
-        
-        
-        
-        
-        
     }
-
     
-
+    func test_likepost() async throws{
+           var service = PostService()
+           
+           do{
+               try await service.UpdatePostLike(userId: "62f503aeaa188d05520baa31", postId: "62f5053aaa188d05520baa33", liked: true)
+           }catch{
+               XCTFail("likepost error \(error)")
+           }
+           
+       }
+    
 }
