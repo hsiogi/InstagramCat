81f2cffe7f897ed5e3c9c8f4b36afd7c7b803f26
hsiogi <hsiogi@168@gmail.com>
2022-08-22 11:24:05 +0800

    新增userpageview

---
+++ InstagramCat/Views/UserPageView.swift
@@ -0,0 +1,88 @@
+//
+//  UserPageView.swift
+//  InstagramCat
+//
+//  Created by 林品均 on 2022/8/18.
+//
+
+import SwiftUI
+
+struct UserPageView: View {
+    
+    @ObservedObject var model = UserPageViewModel()
+    
+    var body: some View {
+        
+        ScrollView{
+            VStack(alignment: .leading){
+                
+                HStack(){
+                    Text(model.user.account)
+                        .font(.title)
+                        .bold()
+                    Spacer()
+                    
+                    Image(systemName: "plus.app")
+                        .imageScale(.large)
+                    
+                    Image(systemName: "list.bullet")
+                        .imageScale(.large)
+                }.padding()
+                
+                HStack(){
+                    
+                    VStack(alignment: .leading){
+                        
+                        ThumbNailView(thumbNail: model.user.thumbNail, width: 80, height: 80)
+                        Text(model.user.name)
+                            .font(.subheadline)
+                        
+                    }.padding(.trailing)
+                    
+                    UserInfoView(postCount: model.postCount, followerCount: model.user.follower_count, followingCount: model.user.following_count)
+                    
+                    
+                    
+                    
+                    
+                }.padding()
+                
+                FavoriteStoriesView()
+                
+                PostGridView(posts: $model.posts)
+                    .task {
+                        await model.getPosts()
+                    }
+                
+                Spacer()
+                
+            }
+        }
+        
+        
+    }
+}
+
+struct UserPageView_Previews: PreviewProvider {
+    
+    
+    static var previews: some View {
+        
+        
+        UserPageView(model: getModel())
+    }
+    
+    
+    static func getModel() -> UserPageViewModel{
+        let postViewModel = PostViewModel()
+        
+        postViewModel.images = ["https://news.agentm.tw/wp-content/uploads/%E6%9C%AA%E5%91%BD%E5%90%8D-1-89-750x422.png"]
+        
+        
+        let model = UserPageViewModel()
+        model.user.account = "coder_marra"
+        model.user.name = "Marra Lin"
+        model.posts = [postViewModel]
+        return model
+    }
+}
