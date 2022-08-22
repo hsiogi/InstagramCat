80c8bf6f6a6e029f6b205650ea72dff6a8911e07
hsiogi <hsiogi@168@gmail.com>
2022-08-22 11:23:23 +0800

    userpageviewmodel 新增取得貼文的方法

--- InstagramCat/ViewModels/UserPageViewModel.swift
+++ InstagramCat/ViewModels/UserPageViewModel.swift
@@ -14,7 +14,33 @@ final class UserPageViewModel : ObservableObject{
     @Published var postCount = 0
     @Published var posts = [PostViewModel]()
     
+    private let userId = MyUserDefaults.userId
+    private let pageCount = 10
     
+    private var postRepository : PostRepositoryProtocol
+    
+    init(postRepository: PostRepositoryProtocol = PostRepository()){
+        self.postRepository = postRepository
+    }
+    
+    @MainActor
+    func getPosts() async {
+        
+        do{
+            
+            print("run")
+            let posts = try await postRepository.GetPosts(userId: self.userId, pageIndex: 1, pageCount: self.pageCount)
+            
+            
+            for post in posts{
+                self.posts.append(PostViewModel(post: post))
+            }
+            
+        }catch{
+            print(error)
+        }
+        
+    }
     
     
     
